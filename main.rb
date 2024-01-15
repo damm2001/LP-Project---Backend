13fde7cf9d82214302765fad0f3ff0ac64746a0e
irvmgarz <83097922+irvmgarz@users.noreply.github.com>
2024-01-14 22:08:33 -0500

    Agregar y visualizar libros

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require './GestorEstudiante'
+require './GestorLibros'
 
 gestor_estudiantes = GestorEstudiante.new([])
-
+gestor_libros = GestorLibros.new([])
 #Meiyin
 
 get '/' do
@@ -13,3 +14,14 @@ get '/' do
 end
 
 #Diego Martinez
+#Irving Macias
+get '/api/libros' do
+    content_type :html
+  
+    gestor_libros.read_data_from_csv
+    libros = gestor_libros.libros.map do |libro|
+      "<li>Titulo: #{libro['titulo']}, Autor: #{libro['autor']}, Edición: #{libro['edicion']}, Disponibilidad: #{libro['disponibilidad']}</li>"
+    end
+  
+    "<h1>Lista de Libros:</h1><ul>#{libros.join}</ul>"
+  end
