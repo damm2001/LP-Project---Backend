08ac1ed161ce51db174c917d08b97d9d9b54e33b
irvmgarz <83097922+irvmgarz@users.noreply.github.com>
2024-01-22 18:43:32 -0500

    agregado valoracion

--- Libro.rb
+++ Libro.rb
@@ -1,10 +1,11 @@
 class Libro
-    attr_accessor :titulo, :autor, :edicion, :disponibilidad
-  
-    def initialize(titulo, autor, edicion, disponibilidad)
-      @titulo = titulo
-      @autor = autor
-      @edicion = edicion
-      @disponibilidad = disponibilidad
-    end
+  attr_accessor :titulo, :autor, :edicion, :disponibilidad, :valoracion
+
+  def initialize(titulo, autor, edicion, disponibilidad, valoracion)
+    @titulo = titulo
+    @autor = autor
+    @edicion = edicion
+    @disponibilidad = disponibilidad
+    @valoracion = valoracion
   end
+end
